e9c590c87042038263ea7b8b9b52f27c859a7e59
doniyor <doniyorkarimoff96@gmail.com>
2022-01-18 00:58:38 +0500

    miner configs

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+WHITENOISE_USE_FINDERS = True
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
